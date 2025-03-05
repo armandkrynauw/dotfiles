f3c46ffec74029dc95d4ec65dd5432fa150d67d9
Armand Krynauw <armand.krynauw@gmail.com>
2025-03-05 20:22:29 +0200

    wezterm: add window padding

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,11 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 1.0
 
+config.window_padding = {
+	top = "20px",
+	bottom = "0px",
+}
+
 -- Misc
 config.automatically_reload_config = true
 config.window_close_confirmation = "NeverPrompt"
